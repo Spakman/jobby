e7c87a4866ce50af52a70a1b8f2dfc6fcfc137cc
Mark Somerville <ms@pccl.info>
2008-04-10 15:22:49 +0100

    * A better Job spec   - Paired with rs

--- spec/jobby_spec.rb
+++ spec/jobby_spec.rb
@@ -2,10 +2,11 @@ require 'activerecord'
 
 module Jobby
   class Job < ActiveRecord::Base
+
     def self.add(freelancer, time_to_live, priority, *args)
       job = self.new
       job.time_to_live = time_to_live
-      job.args = Marshal.dump(args)
+      job.args = args
       job.status = "NEW"
       job.save!
       return job
@@ -16,6 +17,27 @@ module Jobby
       job.update_attributes(:status => "RUNNING", :started_at => Time.now)
       return job
     end
+
+    def running?
+      self.status == 'RUNNING'
+    end
+
+    def progress_message=(message)
+      write_attribute(:progress_message, message)
+      update_attribue(:progress_message, message)
+    end
+
+    def args
+      unless @unmasrshalled_args
+        @unmarshalled_args = Marshal.load(read_attribute(:args)).flatten
+      end
+      @unmarshalled_args
+    end
+
+    def args=(*something)
+      write_attribute(:args, Marshal.dump(something))
+    end
+
   end
 end
 
@@ -27,6 +49,7 @@ describe Jobby::Job do
       :database => "db/jobby.sqlite3",
       :adapter => "sqlite3"
     )
+    Jobby::Job.delete_all
   end
 
   it "should be added to the database" do
@@ -47,5 +70,15 @@ describe Jobby::Job do
     job.started_at.should_not be_nil
   end
 
-  it "should be able to have the progress updated"
+  it "should know if it's running or not" do
+    high_priority_job = Jobby::Job.add(:test_freelancer, 60, 1, [])
+    job = Jobby::Job.next
+    job.running?.should eql(true)
+  end
+
+  it "should automatically marshall args" do
+    low_priority_job = Jobby::Job.add(:test_freelancer, 60, 5, { :cheese => :good })
+    job = Jobby::Job.find(low_priority_job.id)
+    job.args.first[:cheese].should eql(:good)
+  end
 end
