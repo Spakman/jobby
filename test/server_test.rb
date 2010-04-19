86d30d4fd1bfdc5e13f1fdcaaa694eb8477b554a
Ryan Stenhouse <rs@pccl.info>
2010-04-19 11:00:31 +0100

    All specs ported to Test::Unit now

--- test/server_test.rb
+++ test/server_test.rb
@@ -79,4 +79,81 @@ class ServerTest < JobbyTestCase
     assert_equal message, File.read(@child_filepath)
   end
 
+  def test_forks_off_a_child_and_runs_the_specified_code_upon_connection
+    Jobby::Client.new(@socket) { |c| c.send("hiya") }
+    wait_for_jobby!
+    assert_not_equal @server_pid.to_s, File.read(@child_filepath)
+  end
+
+  def test_should_only_fork_of_a_maximum_of_chilren_and_queue_the_rest
+    terminate_server
+    run_server(@socket, @max_child_processes, @log_filepath) do
+      wait_for_jobby! 2
+      File.open(@child_filepath, "a+") do |file|
+        file << "#{Process.pid}\n"
+      end
+    end
+    (@max_child_processes + 2).times do |i|
+      Thread.new do
+        Jobby::Client.new(@socket) { |c| c.send("hiya") }
+      end
+    end
+    wait_for_jobby! 2.5
+    assert_equal @max_child_processes, File.readlines(@child_filepath).length
+    wait_for_jobby! 4
+    assert_equal (@max_child_processes + 2), File.readlines(@child_filepath).length
+  end
+
+  def test_stops_server_and_reaps_children_on_USR1_signal_receipt
+    terminate_server_and_wait_for_jobby!
+    run_server(@socket, 1, @log_filepath) do
+      sleep 3
+    end
+    2.times do |i|
+      Jobby::Client.new(@socket) { |c| c.send("hiya") }
+    end
+    wait_for_jobby!
+    Process.kill "USR1", @server_pid
+    wait_for_jobby! 1.5
+    assert_raise Errno::ECONNREFUSED do
+      lambda { Jobby::Client.new(@socket) { |c| c.send("hello?") } }.call
+    end
+    wait_for_jobby! 5
+    assert_raise Errno::ENOENT do
+      lambda { Jobby::Client.new(@socket) { |c| c.send("hello?") } }.call
+    end
+    assert_equal '2', `pgrep -f 'jobby spec' | wc -l`.strip
+    assert_equal '1', `pgrep -f 'jobbyd spec' | wc -l`.strip
+  end
+
+  def test_can_execute_a_ruby_file_before_forking
+    terminate_server
+    ruby_file_path = File.expand_path(File.dirname(__FILE__) + '/file_for_prerunning.rb')
+    assert File.exists?(ruby_file_path)
+    run_server(@socket, 1, @log_filepath, Proc.new { |logger| load ruby_file_path }) do
+      sleep 2
+      if defined?(Preran)
+        File.open(@child_filepath, "a+") do |file|
+          file << "preran OK"
+        end
+      end
+    end
+    wait_for_jobby!
+    Jobby::Client.new(@socket) { |c| c.send("hiya") }
+    wait_for_jobby! 3
+    assert_equal 'preran OK', File.read(@child_filepath)
+  end
+  
+  def test_closes_all_inherited_file_descriptors_from_calling_processes
+    terminate_server
+    ruby_file_path = File.expand_path(File.dirname(__FILE__) + '/file_for_prerunning.rb')
+    f = File.open(ruby_file_path, "r")
+    run_server(@socket, 1, @log_filepath) do
+      sleep 2
+    end
+    wait_for_jobby! 0.5
+    assert_equal 7, Dir.entries("/proc/#{@server_pid}/fd/").length
+    f.close
+  end
+
 end
